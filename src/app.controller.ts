ffe3da5a7b618de9d678e970cebdace7ba879736
Paolo Soncco <paholo2054@gmail.com>
2023-04-18 09:55:23 -0500

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): { message: string } {
+  getHello(): string {
     // return this.appService.getHello();
-    return { message: 'Probando socket.io' };
+    return 'Hello World!';
   }
 }
